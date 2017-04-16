2e1980785529c25ce982139c074832e5e45075b7
mileberzhang <mileberzhang@gmail.com>
2017-04-16 12:43:53 +0800

    fix utf8

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-db = SQLAlchemy(app，use_native_unicode="utf8")
+db = SQLAlchemy(app, use_native_unicode="utf8")
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
